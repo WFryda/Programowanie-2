663ce61427988cf31e9dd522dec92647be6e4d1d
WFryda <Wojtek.Fryda@gmail.com>
2020-03-28 16:15:04 +0100

    Zdanie_1

--- Programowanie_Z1/Programowanie_Z1/Program.cs
+++ Programowanie_Z1/Programowanie_Z1/Program.cs
@@ -10,17 +10,11 @@ namespace ConsoleApp
             double wynik = 0.0;
             string rownanie;
             Mat.OdwrotnaNotacjaPolska onp = new Mat.OdwrotnaNotacjaPolska();
-            //for (int i = 1; i < args.Length; i++)
-            //{
-            //    liczby[i-1] = char.Parse(args[i]);
-            //}
-            rownanie=args[0].Replace("x",args[1]);
+            rownanie = args[0].Replace("x", args[1]);
             onp.Parse(rownanie);
             Console.WriteLine(onp.ZamianaWyrazenia);
             Console.WriteLine(onp.WyrazeniePostfixowe);
             wynik = onp.Ocenianie();
-            //Console.WriteLine(onp.ZamianaWyrazenia);
-            //Console.WriteLine(onp.WyrazeniePostfixowe);
             Console.WriteLine(wynik);
             int ilosc = int.Parse(args[4]);
             double[] liczby = new double[ilosc];
@@ -42,59 +36,6 @@ namespace ConsoleApp
                 Console.WriteLine("{0} => {1}", liczby2[i], wynik);
             }
 
-            //onp.Parse("3+4*2/(1-5)^2^3");
-            //wynik = onp.Ocenianie();
-            //Console.WriteLine("oryginał: {0}", onp.OrygnialneWyrazenie);
-            //Console.WriteLine("zamiana: {0}", onp.ZamianaWyrazenia);
-            //Console.WriteLine("postfix: {0}", onp.WyrazeniePostfixowe);
-            //Console.WriteLine("wynik: {0}", wynik);
-
-            //wynik = 0.0;
-            //Console.WriteLine();
-            //onp.Parse("exp(sin(1+2))-100");
-            //wynik = onp.Ocenianie();
-            //Console.WriteLine("oryginał: {0}", onp.OrygnialneWyrazenie);
-            //Console.WriteLine("zamiana: {0}", onp.ZamianaWyrazenia);
-            //Console.WriteLine("postfix: {0}", onp.WyrazeniePostfixowe);
-            //Console.WriteLine("wynik: {0}", wynik);
-
-            //Console.WriteLine();
-            //onp.Parse("abs(10)");
-            //wynik = onp.Ocenianie();
-            //Console.WriteLine("oryginał: {0}", onp.OrygnialneWyrazenie);
-            //Console.WriteLine("zamiana: {0}", onp.ZamianaWyrazenia);
-            //Console.WriteLine("postfix: {0}", onp.WyrazeniePostfixowe);
-            //Console.WriteLine("wynik: {0}", wynik);
-            //Console.WriteLine("");
-
-
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //for (int i = 1; i <= 5; i++)
-            //{
-            //    string wyrazenie = "sin2*";
-            //    wyrazenie += i;
-            //    Oblicz(wyrazenie);
-            //}
-
-            Console.WriteLine("Koniec programu");
-            Console.ReadLine();
-
-
-        }
-
-        static void Oblicz(string wyrazenie)
-        {
-            double wynik;
-            Mat.OdwrotnaNotacjaPolska onp = new Mat.OdwrotnaNotacjaPolska();
-            Console.WriteLine();
-            onp.Parse(wyrazenie);
-            wynik = onp.Ocenianie();
-            Console.WriteLine("oryginał: {0}", onp.OrygnialneWyrazenie);
-            Console.WriteLine("zamiana: {0}", onp.ZamianaWyrazenia);
-            Console.WriteLine("postfix: {0}", onp.WyrazeniePostfixowe);
-            Console.WriteLine("wynik: {0}", wynik);
-            Console.WriteLine("");
         }
     }
 }
